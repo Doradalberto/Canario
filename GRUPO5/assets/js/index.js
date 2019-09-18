aa624fba43474567f36646103223f9298005076e
Gabriela Giosa <gabichoichit@gmail.com>
2019-09-18 09:06:54 -0300

    index

--- GRUPO5/assets/js/index.js
+++ GRUPO5/assets/js/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
+
   let db = coDesConnect('https://canario-957b5.firebaseio.com/')
 
   db.download('/', function(data) {
@@ -6,13 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     coDesReplace('.ul', context)
     coDesReplace('.item1', context)
+    coDesReplace('item4', context)
+    
 
 
-    let gabi = data['portfolio'][value]
+    let gabi = data['portfolio']
     coDesReplace('container3', gabi)
 
-    coDesReplace('.item3', context)
-    coDesReplace('.item2', context)
+    coDesReplace('.item3', gabi)
+    coDesReplace('.item2', gabi)
 
   })
 })
