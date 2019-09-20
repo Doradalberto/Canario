52b5cbf44945f92b9aea0b6f4abc257eeb529a22
Dora <isadorasd@al.insper.edu.br>
2019-09-20 09:13:20 -0300

    pasta

--- Modelo/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let db = coDesConnect('https://console.firebase.google.com/project/canario-957b5/database/canario-957b5/data')
-
-  db.download('/', function(data) {
-    context = data
-    coDesReplace('.menu-list', context)
-    coDesReplace('.section-list', context)
-  })
-})
